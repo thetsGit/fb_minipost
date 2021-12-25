cedd2f25b407be5763417d4b9b8d4c1dbef02bf3
Thethan <eagleking495@gmail.com>
2021-12-25 11:27:16 +0630

    uploading 2 github 4 the firsttime

---
+++ app.js
@@ -0,0 +1,8 @@
+const captionBox = ['We are born to be real, not perfect.','A girl should be like a butterfly. Pretty to see, hard to catch.','Turning my dreams into my vision and my vision into reality.','Sand in my toes and saltwater in my curls.','I am a girl. I don’t smoke, drink or party every weekend. And I don’t sleep around or start drama to get attention. Yes, we do still exist!'];
+// const profileBox = ['images/']
+const randomGenerator = (maxNum) => {
+   return Math.floor(Math.random() * maxNum);
+}
+const htmlGenerator = (){
+    return ''
+}
